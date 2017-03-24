d5b776ff4644f6c254f5fc9c6558c09b1d9b0120
diego-sousa-st <diegosousa.st@gmail.com>
2017-03-24 13:13:52 -0300

    backup das procedures sql

---
+++ Site/AlterarUser_jogo_e_Adm.sql
@@ -0,0 +1,91 @@
+#CRIACAO PROCEDURA QUE ALTERA USUARIO COMUM
+
+DELIMITER //
+CREATE PROCEDURE ATT_USER(EmailN VARCHAR(45),PNomeN VARCHAR(20),UNomeN VARCHAR(20),
+	SenhaN VARCHAR(100), DataNascimentoN VARCHAR(10),StilPointsN SMALLINT(3),FotoPerfilN VARCHAR(100))
+BEGIN
+	DECLARE dataNasc Date;
+	SET dataNasc = date_format(str_to_date(DataNascimentoN,'%d/%m/%Y'),'%Y%m%d');
+	UPDATE USUARIO_COMUM 
+	SET Email = EmailN,
+		PNome = PNomeN,
+		UNome = UNomeN,
+		Senha = SenhaN,
+		DataNascimento = dataNasc,
+		ImagemPerfil = FotoPerfilN 	
+	WHERE PEmail = Email;	 
+END //
+DELIMITER ;
+
+#CRIACAO PROCEDURE ALTERACAO ADMIN
+
+
+DELIMITER //
+CREATE PROCEDURE ATT_ADMINISTRADOR(EmailN VARCHAR(45),PNomeN VARCHAR(20),UNomeN VARCHAR(20),
+	SenhaN VARCHAR(100), DataNascimentoN VARCHAR(10),SalarioN SMALLINT(3),CpfN BIGINT(11),RootN BOOLEAN,
+	FotoPerfilN VARCHAR(100))
+BEGIN
+	DECLARE dataNasc Date;
+	SET dataNasc = date_format(str_to_date(DataNascimento,'%d/%m/%Y'),'%Y%m%d');
+	UPDATE USUARIO_COMUM 
+	SET Email = EmailN,
+		PNome = PNomeN,
+		UNome = UNomeN,
+		Senha = SenhaN,
+		DataNascimento = dataNasc,
+		ImagemPerfil = FotoPerfilN,
+		Salario = SalarioN,
+		Cpf = CpfN,
+		Root = RootN,
+		FotoPerfil = FotoPerfilN
+	WHERE PEmail = Email; 	
+END //
+DELIMITER ;
+
+#CRIACAO PROCEDURE ALTERACAO JOGO
+
+DELIMITER //
+CREATE PROCEDURE ATT_JOGO(
+	CodigoJogoN INT UNSIGNED,
+	QtdVendidaN INT UNSIGNED,
+	NotaUsuarioN TINYINT UNSIGNED,
+	ClassificacaoEtariaN TINYINT UNSIGNED,
+	PrecoCustoN FLOAT(5,2) UNSIGNED,
+	PrecoVendaN FLOAT(5,2) UNSIGNED,
+	GeneroN ENUM('Acao', 'Comedia', 'Aventura', 'Romance', 'Drama', 'Terror', 'Suspense', 'Infantil', 'Outro'),
+	NomeN VARCHAR(45),
+	DataLancamentoN VARCHAR(10),
+	IdiomaAudioN VARCHAR(45),
+	IdiomaLegendaN VARCHAR(45),
+	DescricaoN VARCHAR(500),
+	QtdJogadoresN TINYINT,
+	SistemasOperacionaisN SET('Windows', 'Linux', 'MacOS'),
+	RequisitosMinimosN VARCHAR(500),
+	RequisitosRecomendadosN VARCHAR(500),
+	FORNECEDOR_NomeN VARCHAR(45),
+	ADMINISTRADOR_EmailN VARCHAR(45))
+BEGIN
+	DECLARE dataLanc Date;
+	SET dataLanc = date_format(str_to_date(DataLancamentoN,'%d/%m/%Y'),'%Y%m%d');
+	UPDATE JOGO
+	SET CodigoJogo = CodigoJogoN,
+		QtdVendida = QtdVendidaN,
+		NotaUsuario = NotaUsuarioN,
+		ClassificacaoEtaria = ClassificacaoEtariaN,
+		PrecoCusto = PrecoCustoN,
+		PrecoVenda = PrecoVendaN,
+		Genero = GeneroN,
+		Nome = NomeN,
+		DataLancamento = dataLanc,		
+		IdiomaAudio = IdiomaAudioN,
+		IdiomaLegenda = IdiomaLegendaN,
+		Descricao = DescricaoN,
+		QtdJogadores = QtdJogadoresN,
+		SistemasOperacionais = SistemasOperacionaisN,
+		RequisitosMinimos = RequisitosMinimosN,
+		RequisitosRecomendados = RequisitosRecomendadosN,
+		FORNECEDOR_Nome = FORNECEDOR_NomeN,
+		ADMINISTRADOR_Email = ADMINISTRADOR_EmailN
+	WHERE CodigoJogo = CodigoJogoN;
+END //
+DELIMITER ;
--- Site/index.php
+++ Site/index.php
@@ -1,52 +1,306 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Steam-Flix</title>        
-        <link href="View/MDB-Free/css/bootstrap.min.css" rel="stylesheet">        
-        <link href="View/MDB-Free/css/style.css" rel="stylesheet">
+<!DOCTYPE HTML>
+<!--
+	TXT by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>SteamFlix</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="View/html5up-txt/assets/css/main.css" />
+		<link rel="stylesheet" href="View/html5up-txt/assets/css/styleTemplate.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+	</head>
+	<body class="homepage">
+		<div id="page-wrapper">
 
-        <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-        <![endif]-->
-    </head>
-    <body>
-        <div class="container-fluid">
-            <header class="row">
-            	<img class="logo" src="Arquivos/Images/logo.png" alt="SteamFlix">
-                <h1 class="mytext">SteamFlix</h1>
-                <div class="col-md-8">
-					<nav class="menuPrin">						
-						<ul>
-							<li><a href="#">Home</a></li>
-							<li><a href="#">Filmes</a></li>
-						    <li><a href="#">Jogos</a></li>
-						    <li><a href="#">Comunidades</a></li>
-						    <li><a href="#">Perfil</a></li>
-						    <li><a href="#">MySpace</a></li>
-						</ul>
-					</nav>
+			<!-- Header -->
+				<header id="header">
+					<div class="logo container">
+						<div>
+							<h1><a href="index.php" id="logo"><img src="Arquivos/Images/logo.png" alt="SteamFlix" class="imglogo"></a></h1>
+							<p>SteamFlix</p>
+						</div>
+					</div>
+				</header>
+
+			<!-- Nav -->
+				<nav id="nav">
+					<ul>
+						<li class="current"><a href="index.php">Home</a></li>						
+						<li><a href="View/">Filmes</a></li>
+						<li><a href="View/">Jogos</a></li>
+						<li><a href="View/">Comunidades</a></li>
+						<li><a href="View/">Perfil</a></li>
+						<li><a href="View/">MySpace</a></li>
+					</ul>
+				</nav>
+
+			<!-- Banner -->
+				<div id="banner-wrapper">
+					<section id="banner">
+						<h2>Bem-Vindo ao StemFlix</h2>
+						<p>Um site incrível onde você encontra todos os seus filmes e jogos prediletos</p>
+						<a href="View/TelaCadastro.php" class="button">Cadastre-se</a>
+					</section>
 				</div>
-				<div class="col-md-4">
-	                <form class="form-search">
-	                    <input type="text" class="input-medium search-query" placeholder="O que você procura?">
-	                    <button type="submit" class="btn">Buscar</button>
-	                </form>                
-	            </div>
-            </header>
-            <div class="row">
-                <div role="main">
-                    <!-- conteudo principal -->
-                </div>
-            </div>
-            <footer class="row">				
-                <p class="mytext">&copy; Copyright 2017 - Diego e Nechelley</p>
-            </footer>
-        </div>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-        <script src="js/bootstrap.min.js"></script>
-    </body>
+
+			<!-- Main -->
+				<div id="main-wrapper">
+					<div id="main" class="container">
+						<div class="row 200%">
+							<div class="12u">
+
+								<!-- Highlight -->
+									<section class="box highlight">
+										<ul class="special">
+											<li><a href="#" class="icon fa-search"><span class="label">Magnifier</span></a></li>
+											<li><a href="#" class="icon fa-tablet"><span class="label">Tablet</span></a></li>
+											<li><a href="#" class="icon fa-flask"><span class="label">Flask</span></a></li>
+											<li><a href="#" class="icon fa-cog"><span class="label">Cog?</span></a></li>
+										</ul>
+										<header>
+											<h2>A random assortment of icons in circles</h2>
+											<p>And some text that attempts to explain their significance</p>
+										</header>
+										<p>
+											Phasellus quam turpis, feugiat sit amet ornare in, hendrerit in lectus. Praesent semper mod quis eget mi. Etiam eu<br />
+											ante risus. Aliquam erat volutpat. Aliquam luctus et mattis lectus amet pulvinar. Nam nec turpis consequat.
+										</p>
+									</section>
+
+							</div>
+						</div>
+						<div class="row 200%">
+							<div class="12u">
+
+								<!-- Features -->
+									<section class="box features">
+										<h2 class="major"><span>A Major Heading</span></h2>
+										<div>
+											<div class="row">
+												<div class="3u 12u(mobile)">
+
+													<!-- Feature -->
+														<section class="box feature">
+															<a href="#" class="image featured"><img src="images/pic01.jpg" alt="" /></a>
+															<h3><a href="#">A Subheading</a></h3>
+															<p>
+																Phasellus quam turpis, feugiat sit amet ornare in, a hendrerit in
+																lectus dolore. Praesent semper mod quis eget sed etiam eu ante risus.
+															</p>
+														</section>
+
+												</div>
+												<div class="3u 12u(mobile)">
+
+													<!-- Feature -->
+														<section class="box feature">
+															<a href="#" class="image featured"><img src="images/pic02.jpg" alt="" /></a>
+															<h3><a href="#">Another Subheading</a></h3>
+															<p>
+																Phasellus quam turpis, feugiat sit amet ornare in, a hendrerit in
+																lectus dolore. Praesent semper mod quis eget sed etiam eu ante risus.
+															</p>
+														</section>
+
+												</div>
+												<div class="3u 12u(mobile)">
+
+													<!-- Feature -->
+														<section class="box feature">
+															<a href="#" class="image featured"><img src="images/pic03.jpg" alt="" /></a>
+															<h3><a href="#">And Another</a></h3>
+															<p>
+																Phasellus quam turpis, feugiat sit amet ornare in, a hendrerit in
+																lectus dolore. Praesent semper mod quis eget sed etiam eu ante risus.
+															</p>
+														</section>
+
+												</div>
+												<div class="3u 12u(mobile)">
+
+													<!-- Feature -->
+														<section class="box feature">
+															<a href="#" class="image featured"><img src="images/pic04.jpg" alt="" /></a>
+															<h3><a href="#">And One More</a></h3>
+															<p>
+																Phasellus quam turpis, feugiat sit amet ornare in, a hendrerit in
+																lectus dolore. Praesent semper mod quis eget sed etiam eu ante risus.
+															</p>
+														</section>
+
+												</div>
+											</div>
+											<div class="row">
+												<div class="12u">
+													<ul class="actions">
+														<li><a href="#" class="button big">Do Something</a></li>
+														<li><a href="#" class="button alt big">Think About It</a></li>
+													</ul>
+												</div>
+											</div>
+										</div>
+									</section>
+
+							</div>
+						</div>
+						<div class="row 200%">
+							<div class="12u">
+
+								<!-- Blog -->
+									<section class="box blog">
+										<h2 class="major"><span>Another Major Heading</span></h2>
+										<div>
+											<div class="row">
+												<div class="9u 12u(mobile)">
+													<div class="content content-left">
+
+														<!-- Featured Post -->
+															<article class="box post">
+																<header>
+																	<h3><a href="#">Here's a really big heading</a></h3>
+																	<p>With a smaller subtitle that attempts to elaborate</p>
+																	<ul class="meta">
+																		<li class="icon fa-clock-o">15 minutes ago</li>
+																		<li class="icon fa-comments"><a href="#">8</a></li>
+																	</ul>
+																</header>
+																<a href="#" class="image featured"><img src="images/pic05.jpg" alt="" /></a>
+																<p>
+																	Phasellus quam turpis, feugiat sit amet ornare in, a hendrerit in lectus. Praesent
+																	semper mod quis eget mi. Etiam sed ante risus aliquam erat et volutpat. Praesent a
+																	dapibus velit. Curabitur sed nisi nunc, accumsan vestibulum lectus. Lorem ipsum
+																	dolor sit non aliquet sed, tempor et dolor.  Praesent a dapibus velit. Curabitur
+																	accumsan.
+																</p>
+																<a href="#" class="button">Continue Reading</a>
+															</article>
+
+													</div>
+												</div>
+												<div class="3u 12u(mobile)">
+													<div class="sidebar">
+
+														<!-- Archives -->
+															<ul class="divided">
+																<li>
+																	<article class="box post-summary">
+																		<h3><a href="#">A Subheading</a></h3>
+																		<ul class="meta">
+																			<li class="icon fa-clock-o">6 hours ago</li>
+																			<li class="icon fa-comments"><a href="#">34</a></li>
+																		</ul>
+																	</article>
+																</li>
+																<li>
+																	<article class="box post-summary">
+																		<h3><a href="#">Another Subheading</a></h3>
+																		<ul class="meta">
+																			<li class="icon fa-clock-o">9 hours ago</li>
+																			<li class="icon fa-comments"><a href="#">27</a></li>
+																		</ul>
+																	</article>
+																</li>
+																<li>
+																	<article class="box post-summary">
+																		<h3><a href="#">And Another</a></h3>
+																		<ul class="meta">
+																			<li class="icon fa-clock-o">Yesterday</li>
+																			<li class="icon fa-comments"><a href="#">184</a></li>
+																		</ul>
+																	</article>
+																</li>
+																<li>
+																	<article class="box post-summary">
+																		<h3><a href="#">And Another</a></h3>
+																		<ul class="meta">
+																			<li class="icon fa-clock-o">2 days ago</li>
+																			<li class="icon fa-comments"><a href="#">286</a></li>
+																		</ul>
+																	</article>
+																</li>
+																<li>
+																	<article class="box post-summary">
+																		<h3><a href="#">And One More</a></h3>
+																		<ul class="meta">
+																			<li class="icon fa-clock-o">3 days ago</li>
+																			<li class="icon fa-comments"><a href="#">8,086</a></li>
+																		</ul>
+																	</article>
+																</li>
+															</ul>
+															<a href="#" class="button alt">Browse Archives</a>
+
+													</div>
+												</div>
+											</div>
+										</div>
+									</section>
+
+							</div>
+						</div>
+					</div>
+				</div>
+
+			<!-- Footer -->
+				<footer id="footer" class="container">
+					<div class="row 200%">
+						<div class="12u">
+
+							<!-- About -->
+								<section>
+									<h2 class="major"><span>O que é o SteamFlix?</span></h2>
+									<p>
+										SteamFlix é <strong>a revolução</strong>, para os que curtem 
+										algum tipo de jogo e/ou filme e não querem ficar comprando
+										os produtos de um determinado peersonagem ou série em lugares diferentes. Aqui tudo isso pode ser feito em um único lugar
+										Apreveite e divirta-se :)
+									</p>
+								</section>
+
+						</div>
+					</div>
+					<div class="row 200%">
+						<div class="12u">
+
+							<!-- Contact -->
+								<section>
+									<h2 class="major"><span>Entre em contato conosco!</span></h2>
+									<ul class="contact">
+										<li><a class="icon fa-facebook" href="#"><span class="label">Facebook</span></a></li>
+										<li><a class="icon fa-twitter" href="#"><span class="label">Twitter</span></a></li>
+										<li><a class="icon fa-instagram" href="#"><span class="label">Instagram</span></a></li>
+										<li><a class="icon fa-dribbble" href="#"><span class="label">Dribbble</span></a></li>
+										<li><a class="icon fa-google-plus" href="#"><span class="label">Google+</span></a></li>
+									</ul>
+								</section>
+
+						</div>
+					</div>
+
+					<!-- Copyright -->
+						<div id="copyright">
+							<ul class="menu">
+								<li>Designed and Developed by Diego Sousa e Nechelley Alves using html5up-txt &copy; 2016-2017. All rights reserved</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+							</ul>
+						</div>
+
+				</footer>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/skel-viewport.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
 </html>
