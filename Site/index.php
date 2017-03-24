fd4a3390cfe3fb557002c95d293651fabf27c3cb
diego-sousa-st <diegosousa.st@gmail.com>
2017-03-24 03:26:03 -0300

    começo views

---
+++ Site/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Steam-Flix</title>        
+        <link href="View/MDB-Free/css/bootstrap.min.css" rel="stylesheet">        
+        <link href="View/MDB-Free/css/style.css" rel="stylesheet">
+
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <div class="container-fluid">
+            <header class="row">
+                <h1 class="mytext">SteamFlix</h1>
+                <div class="menubar">
+					<nav class="navbar-fixed-top">						
+						<ul>
+							<li><a href="#">Home</a></li>
+							<li><a href="#">Filmes</a></li>
+						    <li><a href="#">Jogos</a></li>
+						    <li><a href="#">Comunidades</a></li>
+						    <li><a href="#">Perfil</a></li>
+						    <li><a href="#">MySpace</a></li>
+						</ul>
+					</nav>
+	                <form class="form-search">
+	                    <input type="text" class="input-medium search-query" placeholder="O que você procura?">
+	                    <button type="submit" class="btn">Buscar</button>
+	                </form>
+                </div>
+            </header>
+            <div class="row">
+                <div role="main">
+                    <!-- conteudo principal -->
+                </div>
+            </div>
+            <footer class="row">				
+                <p class="mytext">&copy; Copyright 2017 - Diego e Nechelley</p>
+            </footer>
+        </div>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+    </body>
+</html>
