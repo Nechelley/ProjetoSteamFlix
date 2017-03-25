1294c26880314fb58cb356d3164c2030d87efb0a
diego-sousa-st <diegosousa.st@gmail.com>
2017-03-25 16:47:11 -0300

    alteração e exclusao de usuario

--- Site/View/telaPerfil.php
+++ Site/View/telaPerfil.php
@@ -13,14 +13,24 @@
 
 <!-- Highlight -->
 	<section class="box highlight">
+		<div class="formulario">
+		<h1 class="titulo">Perfil</h1>
+		<div clas="formulario">
+			<form class="formularioBotao" action="../Controller/alterarUsuario.php" method="post">
+			<input type="submit" value="Alterar Dados">
+			</form>
+			<form class="formularioBotao"action="../Controller/deletarUsuario.php" method="post">
+				<input type="submit" value="Deletar Perfil">
+			</form>
+		</div>		
 		<form action="NADA.php" method="POST">
 			Primeiro Nome: <input type="text" name="pNome" value="<?php echo $pNome;?>" readonly> <br/>
 			Segundo Nome: <input type="text" name="uNome"  value="<?php echo $uNome;?>" readonly> <br/>
 			Email: <input type="text" name="email" value="<?php echo $email;?>" readonly> <br/>
 			Data de Nascimento: <input type="text" name="dataNascimento"  value="<?php echo $data;?>" readonly> <br/>
-			Foto de Perfil: <img src="../Arquivos/FotosPerfil/<?php echo $img; ?>" alt="Foto perfil" width="50px" height="50px"> <br/><br/>
+			Foto de Perfil: <img class="fotoPerfil"src="../Arquivos/FotosPerfil/<?php echo $img; ?>" alt="Foto perfil"> <br/><br/>
 		</form>
-		
+		</div>
 	</section>
 
 </div>
